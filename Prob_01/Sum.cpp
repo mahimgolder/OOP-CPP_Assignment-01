95b6addbfe351c3b96c24a4caf5cf3e09540bb9a
Mahim Golder <mahimgolder@gmail.com>
2026-03-02 00:31:51 +0600

    Added the Problem 01

---
+++ Prob_01/Sum.cpp
@@ -0,0 +1,21 @@
+// Prob:1. Sum of two numbers.(Take those numbers from user)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
